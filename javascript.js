268e288812500e8b20eb11f71efa7a5c8055f0bf
AmirSharofov <mister.amir.777@gmail.com>
2024-04-29 11:46:31 +0500

    change js code in javascript

--- javascript.js
+++ javascript.js
@@ -1,19 +1,32 @@
-let money = 10000;
-let account = 7777;
-let name = 'Alex';
-
-const abc = prompt('Как вас зовут?').toLowerCase().trim();
-
-if (abc === 'alex') {
-    const ask = prompt('Номер счета?');
-    if (ask === account.toString()) {
-        const ppp = prompt('Сколько обноличить?')
-        if(ppp <= 10000){
-        confirm('Все отлично')
-        prompt('Сколько снял? Сколько обноличивать?')
-        
-        }else{alert('Недостаточно денег')}
-    } else { alert('Пользователь не найден, досвидули') }
-} else { alert('Пользователь не найден, досвидули') }
+let str1 = `aaa@bbb@ccc`
+let arr1 = str1.split("@").join("!")
+console.log(arr1)
 
 
+let str2 = "HELLOWORLD"
+let result2 = str2[0].toUpperCase() + str2.slice(1).toLowerCase()
+console.log(result2)
+
+let str3 = 'Hello, it is HTML'
+let result3 = str3.split(',').join('').split('HTML').join('not JS')
+console.log(result3)
+
+let str4 = 'let a = "alex"'
+let result4 = str4[0].toUpperCase() + str4.slice(1)
+console.log(result4);
+
+let a = `alex`;
+let result5 = a.charAt(0).toUpperCase() +  a.slice(1);
+console.log(result5)
+
+
+let resultNum = String(Math.random()).split('.').join("")
+console.log(resultNum)
+
+
+let txt = "Lex Luter has a big suit"
+
+console.log(
+    txt.slice(txt.search("a"), txt.search("a") + "a".length).toUpperCase() + 
+    txt.slice(txt.search("Lex"), txt.search("Lex") + "Lex".length).toLowerCase()
+)
