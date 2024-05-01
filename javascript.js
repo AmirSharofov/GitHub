fb618772b7bc8e76a9cca285e43a823f7b735503
AmirSharofov <mister.amir.777@gmail.com>
2024-05-01 14:59:25 +0500

    change js code in javascript.js

--- javascript.js
+++ javascript.js
@@ -1,32 +1,18 @@
-let str1 = `aaa@bbb@ccc`
-let arr1 = str1.split("@").join("!")
-console.log(arr1)
+const name = prompt('Name: ');
 
+if (name.indexOf(' ') !== -1 && name.trim().length > 7) {
+    alert('Access denied');
+} else {
+    if (name.length < 10) {
+        const age = +prompt('Age: ');
 
-let str2 = "HELLOWORLD"
-let result2 = str2[0].toUpperCase() + str2.slice(1).toLowerCase()
-console.log(result2)
-
-let str3 = 'Hello, it is HTML'
-let result3 = str3.split(',').join('').split('HTML').join('not JS')
-console.log(result3)
-
-let str4 = 'let a = "alex"'
-let result4 = str4[0].toUpperCase() + str4.slice(1)
-console.log(result4);
-
-let a = `alex`;
-let result5 = a.charAt(0).toUpperCase() +  a.slice(1);
-console.log(result5)
-
-
-let resultNum = String(Math.random()).split('.').join("")
-console.log(resultNum)
-
-
-let txt = "Lex Luter has a big suit"
-
-console.log(
-    txt.slice(txt.search("a"), txt.search("a") + "a".length).toUpperCase() + 
-    txt.slice(txt.search("Lex"), txt.search("Lex") + "Lex".length).toLowerCase()
-)
+        if (age > 18) {
+            arr.push({
+                name: name,
+                age: age
+            });
+        }
+    } else {
+        alert('Access denied');
+    }
+}
